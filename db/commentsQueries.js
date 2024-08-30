3afd1e4ecc21ab8a796738c404362050a797f19a
gabrieleinsfeld <gabrieleinsfeld@gmail.com>
2024-08-30 11:18:51 -0300

    Added comment CRUD functions and routes

--- db/commentsQueries.js
+++ db/commentsQueries.js
@@ -28,7 +28,50 @@ const createComment = async (content, postId, userId) => {
   }
 };
 
+const updateComment = async (id, content, userId) => {
+  try {
+    const comment = await prisma.comment.findUnique({
+      where: { id },
+    });
+    if (!comment) {
+      throw new Error("Comment not found");
+    }
+    if (comment.userId !== userId) {
+      throw new Error("You are not authorized to update this comment");
+    }
+    const updatedComment = await prisma.comment.update({
+      where: { id },
+      data: { content },
+    });
+    return updatedComment;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const deleteComment = async (id, userId) => {
+  try {
+    const comment = await prisma.comment.findUnique({
+      where: { id },
+    });
+    if (!comment) {
+      throw new Error("Comment not found");
+    }
+    if (comment.userId !== userId) {
+      throw new Error("You are not authorized to update this comment");
+    }
+    const deletedComment = await prisma.comment.delete({
+      where: { id },
+    });
+    return deletedComment;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
 module.exports = {
   getCommentsByPostId,
   createComment,
+  updateComment,
+  deleteComment,
 };
--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -19,4 +19,26 @@ commentRouter.post("/", async (req, res, next) => {
   }
 });
 
+commentRouter.put("/", async (req, res, next) => {
+  const { commentId, content } = req.body;
+  const userId = req.user.id;
+  try {
+    const comment = await db.updateComment(commentId, content, userId);
+    res.json(comment);
+  } catch (error) {
+    next(error);
+  }
+});
+
+commentRouter.delete("/:commentId", async (req, res, next) => {
+  const commentId = req.params.commentId;
+  const userId = req.user.id;
+  try {
+    const comment = await db.deleteComment(commentId, userId);
+    res.json({ comment });
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = commentRouter;
